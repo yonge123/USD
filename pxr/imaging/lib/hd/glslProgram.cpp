061c2e059cb682abe3b89aba9cee94b3883c67dc
Pal Mezei <sirpalee@gmail.com>
2016-12-08 14:28:04 +1100

    Removing debug code accidentally left in glslProgram.cpp.

--- pxr/imaging/lib/hd/glslProgram.cpp
+++ pxr/imaging/lib/hd/glslProgram.cpp
@@ -72,8 +72,6 @@ HdGLSLProgram::ComputeHash(TfToken const &sourceFile)
     return hash;
 }
 
-#include <map>
-
 bool
 HdGLSLProgram::CompileShader(GLenum type,
                              std::string const &shaderSource)
@@ -122,14 +120,9 @@ HdGLSLProgram::CompileShader(GLenum type,
         _program.SetAllocation(program, 0);
     }
 
-    static std::map<int, int> shader_counter;
     // create a shader, compile it
     const char *shaderSources[1];
     shaderSources[0] = shaderSource.c_str();
-    std::stringstream ss;
-    ss << "/home/palm/data/usd/shaders/" << (shaderType + 3) << "_" << shader_counter[type]++ << ".glsl";
-    std::fstream fs(ss.str(), std::ios::out);
-    fs << shaderSource;
     GLuint shader = glCreateShader(type);
     glShaderSource(shader, sizeof(shaderSources)/sizeof(const char *), shaderSources, NULL);
     glCompileShader(shader);
