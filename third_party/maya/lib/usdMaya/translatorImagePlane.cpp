a26fa2e5c63da84d21af15f6d0b7aa4acc8ba161
Pal Mezei <sirpalee@gmail.com>
2017-01-06 14:32:44 +1100

    Finding the right paths for the camera shape when creating the image plane.

--- third_party/maya/lib/usdMaya/translatorImagePlane.cpp
+++ third_party/maya/lib/usdMaya/translatorImagePlane.cpp
@@ -1,9 +1,13 @@
 #include "translatorImagePlane.h"
 
+#include <maya/MFnDependencyNode.h>
+#include <maya/MFnCamera.h>
+
 bool PxrUsdMayaTranslatorImagePlane::Read(
     const UsdGeomImagePlane& usdImagePlane,
     MObject parentNode,
     const PxrUsdMayaPrimReaderArgs& args,
     PxrUsdMayaPrimReaderContext* context) {
+
     return true;
 }
