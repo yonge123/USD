83d9d1e32f9590efb617c02e69912b18a4a9d976
Paul Molodowitch <pm@stanfordalumni.org>
2017-01-11 14:55:24 -0800

    animation for sub-refs

--- third_party/maya/lib/usdMaya/translatorModelAssembly.cpp
+++ third_party/maya/lib/usdMaya/translatorModelAssembly.cpp
@@ -473,6 +473,12 @@ PxrUsdMayaTranslatorModelAssembly::Read(
                    + parentName
                    + ";parentRefPaths="
                    + joinedParentRefs.c_str()
+                   + ";readAnimData="
+                   + int(args.GetReadAnimData())
+                   + ";startTime="
+                   + args.GetStartTime()
+                   + ";endTime="
+                   + args.GetEndTime()
                    + "\" \""
                    + refPath.c_str()
                    + "\";");
