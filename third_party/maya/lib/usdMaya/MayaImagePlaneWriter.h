5a367e9583c612cc036023b5fe143bab25bb4fe9
Pal Mezei <sirpalee@gmail.com>
2017-02-03 12:26:13 +1100

    Improving code quality.

--- third_party/maya/lib/usdMaya/MayaImagePlaneWriter.h
+++ third_party/maya/lib/usdMaya/MayaImagePlaneWriter.h
@@ -16,8 +16,8 @@ class MayaImagePlaneWriter : public MayaPrimWriter {
 public:
     MayaImagePlaneWriter(MDagPath& iDag, UsdStageRefPtr stage, const JobExportArgs& iArgs);
     virtual ~MayaImagePlaneWriter() {};
-    virtual UsdPrim write(const UsdTimeCode& usdTime);
-    virtual bool isShapeAnimated() const;
+    virtual UsdPrim write(const UsdTimeCode& usdTime) override;
+    virtual bool isShapeAnimated() const override;
 protected:
     bool writeImagePlaneAttrs(const UsdTimeCode& usdTime, UsdGeomImagePlane& primSchema);
 
