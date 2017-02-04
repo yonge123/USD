373b71bb29869f1240670540fc924593fe636edc
orenouard <orenouard@gmail.com>
2017-02-03 16:56:40 -0800

    Code cleaning

--- third_party/maya/plugin/pxrUsd/plugin.cpp
+++ third_party/maya/plugin/pxrUsd/plugin.cpp
@@ -179,8 +179,8 @@ MStatus initializePlugin(
     }
 
     // A MPxCacheFormat to save Maya point data to UsdGeomPoints
-    status = plugin.registerCacheFormat(usdCacheFormat::translatorName(),
-                                        usdCacheFormat::creator);
+    status = plugin.registerCacheFormat(UsdCacheFormat::translatorName(),
+                                        UsdCacheFormat::creator);
 
     if (!status) {
         status.perror("pxrUsd: unable to register USD Cache format.");
