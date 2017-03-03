ac1cdbbd468c3ab5258ea0afe03af9a0f3dcbda9
Pal Mezei <sirpalee@gmail.com>
2017-03-03 11:42:36 +1100

    [usdMaya] Properly handling animation in the vdbVisualizerWriter.

--- third_party/maya/lib/usdMaya/VdbVisualizerWriter.cpp
+++ third_party/maya/lib/usdMaya/VdbVisualizerWriter.cpp
@@ -96,6 +96,10 @@ void VdbVisualizerWriter::write(const UsdTimeCode& usdTime) {
         primSchema.GetDsoAttr().Set(std::string("volume_openvdb"));
     }
 
+    if (usdTime.IsDefault() == getArgs().exportAnimation) {
+        return;
+    }
+
     // The node regenerates all kinds of params, so we always need to write these out.
     const auto out_vdb_path = volume_node.findPlug("outVdbPath").asString();
     const auto& bbox_min = volume_node.findPlug("bboxMin").asMDataHandle().asFloat3();
