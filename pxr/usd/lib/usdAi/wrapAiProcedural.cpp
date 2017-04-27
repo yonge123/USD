49b01895ea4c7a34da3f6b38f7539ad08f387a66
Pal Mezei <sirpalee@gmail.com>
2017-04-27 15:50:11 +1000

    Waaaa.

--- pxr/usd/lib/usdAi/wrapAiProcedural.cpp
+++ pxr/usd/lib/usdAi/wrapAiProcedural.cpp
@@ -106,7 +106,7 @@ void wrapUsdAiProcedural()
         .def("GetDsoAttr",
              &This::GetDsoAttr)
         .def("CreateDsoAttr",
-             &_CreateDsoPXR_NAMESPACE_CLOSE_SCOPEAttr,
+             &_CreateDsoAttr,
              (arg("defaultValue")=object(),
               arg("writeSparsely")=false))
         
