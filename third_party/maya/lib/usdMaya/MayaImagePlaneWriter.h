70b63e10d85ebb01cfca5f62b97f9e1523384541
Pal Mezei <palm@lumapictures.com>
2017-11-13 18:07:37 +1100

    Stuff

--- third_party/maya/lib/usdMaya/MayaImagePlaneWriter.h
+++ third_party/maya/lib/usdMaya/MayaImagePlaneWriter.h
@@ -14,7 +14,7 @@ class UsdGeomImagePlane;
 
 class MayaImagePlaneWriter : public MayaPrimWriter {
 public:
-    MayaImagePlaneWriter(const MDagPath & iDag, const SdfPath& uPath, bool instanceSource, usdWriteJobCtx& jobCtx);
+    MayaImagePlaneWriter(const MDagPath & iDag, const SdfPath& uPath, usdWriteJobCtx& job);
     virtual ~MayaImagePlaneWriter();
     
     virtual void write(const UsdTimeCode& usdTime) override;
