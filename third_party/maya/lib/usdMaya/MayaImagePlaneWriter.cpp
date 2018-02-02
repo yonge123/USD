372d53d99a512962284e0440e9beb90de44a5df3
Paul Molodowitch <pm@stanfordalumni.org>
2018-02-01 18:17:11 -0800

    fix bug with ImagePlane usd-path translation
    to do this, as first step, made ImagePlaneWriter inherit from MayaTransformWriter
    
    also fixed logic for (correctly) removing the camera-shape from the usd path if
    it was merged
    formerly, was off-by-one, due to not accounting for the underworld "root" needing
    to be popped from the dag path; also, new version is more flexible (ie, image plane
    can be an arbitrary number of levels nested under the underworld)

--- third_party/maya/lib/usdMaya/MayaImagePlaneWriter.cpp
+++ third_party/maya/lib/usdMaya/MayaImagePlaneWriter.cpp
@@ -26,53 +26,109 @@ namespace {
 }
 
 MayaImagePlaneWriter::MayaImagePlaneWriter(const MDagPath & iDag, const SdfPath& uPath, bool instanceSource, usdWriteJobCtx& jobCtx)
-    : MayaPrimWriter(iDag, uPath, jobCtx), mIsShapeAnimated(false) {
+    : MayaTransformWriter(iDag, uPath, instanceSource, jobCtx),
+      mIsShapeAnimated(false)
+{
     if (getArgs().exportAnimation) {
         MObject obj = getDagPath().node();
         mIsShapeAnimated = PxrUsdMayaUtil::isAnimated(obj);
     }
 
     if (getArgs().mergeTransformAndShape) {
-        // the path will always look like :
+        // TODO: Replicating logic from MayaTransformWriter... and MDagPathToUsdPath...
+        // combine these!
+        auto hasOnlyOneShapeBelow = [&jobCtx] (const MDagPath& path) {
+            auto numberOfShapesDirectlyBelow = 0u;
+            path.numberOfShapesDirectlyBelow(numberOfShapesDirectlyBelow);
+            if (numberOfShapesDirectlyBelow != 1) {
+                return false;
+            }
+            const auto childCount = path.childCount();
+            if (childCount == 1) {
+                return true;
+            }
+            // Make sure that the other objects are exportable - ie, still want
+            // to collapse if it has two shapes below, but one of them is an
+            // intermediateObject shape
+            MDagPath childDag(path);
+            auto numExportableChildren = 0u;
+            for (auto i = 0u; i < childCount; ++i) {
+                childDag.push(path.child(i));
+                if (jobCtx.needToTraverse(childDag)) {
+                    ++numExportableChildren;
+                    if (numExportableChildren > 1) {
+                        return false;
+                    }
+                }
+                childDag.pop();
+            }
+            return (numExportableChildren == 1);
+        };
+
+        // the path will often look like :
         // camera transform -> camera shape -> image plane transform -> image plane shape
-        // So first we pop the image plane shape if possible,
-        // then we are removing the camera shape
-        auto imagePlaneXform = iDag;
-        imagePlaneXform.pop();
-        auto cameraXform = imagePlaneXform;
-        cameraXform.pop();
 
-        unsigned int imagePlaneShapes = 0;
-        unsigned int cameraShapes = 0;
-        imagePlaneXform.numberOfShapesDirectlyBelow(imagePlaneShapes);
-        cameraXform.numberOfShapesDirectlyBelow(cameraShapes);
+        // Because we are dealing with a underworld, we have the deal with the possibility that
+        // the BOTH the camera shape and image plane shape are merged into their transforms...
+        // ...or only one of those are... or neither are.
 
-        // Use tokens from the already-created path - that way, we don't redo
-        // processing done by MDagPathToUsdPath, and we avoid the hash from
-        // token re-creation
-        if (cameraShapes == 1 || imagePlaneShapes == 1) {
-            SdfPath usdPath;
-            if (cameraShapes == 1) {
-                // Get the cameraShape path, then swap it out for the
-                // imagePlaneXform, effectively removing it
-                auto impagePlaneXformPath = getUsdPath().GetParentPath();
-                usdPath = impagePlaneXformPath.GetParentPath().ReplaceName(
-                        impagePlaneXformPath.GetElementToken());
-            }
-            else {
-                // We just use the camera shape path
-                usdPath = getUsdPath().GetParentPath().GetParentPath();
-            }
+        // Currently, the parent MayaTransformWriter will handle the merging of the imagePlane
+        // shape with the image plane transform.  Now, we just need to worry about (possilby)
+        // popping out the camera shape from our usd path...
 
-            if (imagePlaneShapes == 1) {
-                usdPath.AppendChild(getUsdPath().GetParentPath().GetElementToken());
+        if (getDagPath().pathCount() > 1)
+        {
+            auto cameraXformDag = getDagPath();
+
+            // We're in the underworld, get the first xform before the underworld (should be the
+            // camera's xform)
+
+            // First, a note about dag paths + underworlds - the length of a dag path will be
+            //    mainPath.length() = sub_path_sum + mainPath.pathCount() - 1
+            // where
+            //    sub_path_sum = sum(mainPath.getPath(i) for i in 0<i<mainPath.pathCount())
+            // Why is it not just equal to sub_path_sum? Why do we add in mainPath.pathCount?
+            // Because each "root" node of each underworld counts as a node when iterating up -
+            // That is, if we start with:
+            //    |camXform|camShape->|imagePlaneXform|imagePlaneShape
+            // Then, as we pop paths, we will get:
+            //    |camXform|camShape->|imagePlaneXform
+            //    |camXform|camShape->|
+            //    |camXform|camShape
+            //    |camXform
+            // ...for a total of 5 paths. The oddity is "|camXform|camShape->|", for which there
+            // will NOT be a corresponding element added to the usdPath, so we need to make
+            // sure that is handled correctly!
+
+            // First, get the number of elements in our current underworld:
+            MDagPath underworldDag;
+            cameraXformDag.getPath(underworldDag, cameraXformDag.pathCount() - 1);
+            const auto underworldLength = underworldDag.length();
+
+            // Then pop - note we add two:
+            //    just popping underworldLength will get us to the underworld root, ie, |camXform|camShape->|
+            //    popping one more will get us the camera shape, ie, |camXform|camShape
+            //    popping one more will get us the camera xform, ie, |camXform
+            cameraXformDag.pop(underworldLength + 2);
+
+            // Now we test THIS to see if it is merged...
+            if (hasOnlyOneShapeBelow(cameraXformDag))
+            {
+                // Ok, cameraShape was merged... need to remove the appropriate element from our
+                // usd path...
+
+                // We use tokens from the already-created path - that way, we don't redo
+                // processing done by MDagPathToUsdPath, and we avoid the hash from
+                // token re-creation
+                SdfPath cameraShapePath = getUsdPath();
+                for (auto i = 0u; i < underworldLength; ++i)
+                {
+                    cameraShapePath = cameraShapePath.GetParentPath();
+                }
+                auto cameraXformPath = cameraShapePath.GetParentPath();
+
+                setUsdPath(getUsdPath().ReplacePrefix(cameraShapePath, cameraXformPath));
             }
-            else {
-                usdPath = usdPath
-                        .AppendChild(getUsdPath().GetParentPath().GetElementToken())
-                        .AppendChild(getUsdPath().GetElementToken());
-            }
-            setUsdPath(usdPath);
         }
     }
 
@@ -120,6 +176,9 @@ bool MayaImagePlaneWriter::isShapeAnimated() const {
 }
 
 bool MayaImagePlaneWriter::writeImagePlaneAttrs(const UsdTimeCode& usdTime, UsdGeomImagePlane& primSchema) {
+    // Write parent class attrs
+    writeTransformAttrs(usdTime, primSchema);
+
     if (usdTime.IsDefault() == isShapeAnimated()) {
         return true;
     }
