f504b8e17344a8fef561e8924e505b44b83ea4d3
Pal Mezei <palm@lumapictures.com>
2018-03-09 11:17:19 +1100

    Merge conflict handling.

--- third_party/maya/lib/usdMaya/MayaMeshWriter.cpp
+++ third_party/maya/lib/usdMaya/MayaMeshWriter.cpp
@@ -286,7 +286,7 @@ bool MayaMeshWriter::writeMeshAttrs(const UsdTimeCode &usdTime, UsdGeomMesh &pri
             colorSetNames[i] == "v") {
             _writeMotionVector(primSchema,
                                usdTime,
-                               lMesh,
+                               finalMesh,
                                colorSetNames[i]);
             continue;
         }
