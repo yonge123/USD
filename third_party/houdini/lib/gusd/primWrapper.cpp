804886d5a0b078d6c76beae6cabf5ce2db2b2280
Nathan Rusch <nathanrusch@gmail.com>
2018-09-05 15:23:00 -0700

    [UsdHoudini] Fix syntax error introduced by git stash

--- third_party/houdini/lib/gusd/primWrapper.cpp
+++ third_party/houdini/lib/gusd/primWrapper.cpp
@@ -1183,7 +1183,7 @@ GusdPrimWrapper::loadPrimvars(
             loadPrimvar(stPrimvar, st);
         }
         else if (stPrimvar.GetTypeName() == SdfValueTypeNames->Float2Array ||
-+                stPrimvar.GetTypeName() == SdfValueTypeNames->TexCoord2fArray) {
+                 stPrimvar.GetTypeName() == SdfValueTypeNames->TexCoord2fArray) {
             // Need to read 'st' into 'uv' (and possibly do a type conversion)
             DBG(cerr << "Remapping st to uv with type conversion" << endl);
 
